e35a737e94c2cf7c7c4f9fbb7cb322ffa85c6778
Nathan Jenan <nathan.jenan@polarity.io>
2018-05-10 07:12:28 -0700

    Initial commit

---
+++ integration.js
@@ -0,0 +1,51 @@
+let async = require('async');
+let config = require('./config/config');
+
+let Logger;
+let requestOptions = {};
+
+function getRequestOptions() {
+    return JSON.parse(JSON.stringify(requestOptions));
+}
+
+function doLookup(entities, options, callback) {
+    callback(null, null);
+}
+
+function startup(logger) {
+    Logger = logger;
+
+    if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
+        requestOptions.cert = fs.readFileSync(config.request.cert);
+    }
+
+    if (typeof config.request.key === 'string' && config.request.key.length > 0) {
+        requestOptions.key = fs.readFileSync(config.request.key);
+    }
+
+    if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
+        requestOptions.passphrase = config.request.passphrase;
+    }
+
+    if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
+        requestOptions.ca = fs.readFileSync(config.request.ca);
+    }
+
+    if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
+        requestOptions.proxy = config.request.proxy;
+    }
+
+    if (typeof config.request.rejectUnauthorized === 'boolean') {
+        requestOptions.rejectUnauthorized = config.request.rejectUnauthorized;
+    }
+}
+
+function validateOptions(options, callback) {
+    callback(null, null);
+}
+
+module.exports = {
+    doLookup: doLookup,
+    startup: startup,
+    validateOptions: validateOptions
+};
